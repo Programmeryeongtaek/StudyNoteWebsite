d64db42be14e509e61d3b831d04420c88f74ec91
YeongTaek <yeongtaek2109@gmail.com>
2023-07-08 18:34:03 +0900

    초기셋팅

---
+++ pages/CardNews.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CardNews = () => {
+  return (
+    <div>
+      <div>
+        <button>카드 생성</button>
+      </div>
+      <hr />
+      <div>
+        카드보기
+      </div>
+    </div>
+  )
+}
+
+export default CardNews;
