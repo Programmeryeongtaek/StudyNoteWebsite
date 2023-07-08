7cdb9f6d716554c06e2f55c9c00a6508a08f437e
YeongTaek <yeongtaek2109@gmail.com>
2023-07-08 18:34:23 +0900

    ImageFileInput 구성

---
+++ components/CreateCard/ImageFileInput.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from 'react';
+import placeholderImg from '@/public/images/preview-placeholder.png';
+
+const ImageFileInput = ({ className = '', initialPreview, name, value, onChange }) => {
+  const [preview, setPreview] = useState(initialPreview);
+  const inputRef = useRef();
+
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
+  };
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = '';
+    onChange(name, null);
+  };
+
+  useEffect(() => {
+    if (!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview(initialPreview);
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value, initialPreview]);
+
+  return (
+    <div className={`ImageFileInput ${className}`}>
+      <img className={`Image-preview ${preview ? 'selected' : ''}`} src={preview || placeholderImg} alt="이미지 미리보기" />
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && (
+        <button
+          className=''
+          type='button'
+          onClick={handleClearClick}
+        >
+
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default ImageFileInput
