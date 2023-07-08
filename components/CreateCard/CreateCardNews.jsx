8ef716324d48d61702bcdf79b009e1b6bf5d92cd
YeongTaek <yeongtaek2109@gmail.com>
2023-07-08 18:34:52 +0900

    CardNews 구성

---
+++ components/CreateCard/CreateCardNews.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CreateCardForm from '@/components/CreateCard/CreateCardForm';
+
+
+
+const CreateCardNews = () => {
+  return (
+    <div>
+      <div>
+        <CreateCardForm />
+      </div>
+
+      <hr />
+      <div>
+        <div>
+          미리보기
+        </div>
+        <div className='border-black' >
+          <button className='border-4'>작성하기</button>
+          <button className='border-4'>취소</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateCardNews;
